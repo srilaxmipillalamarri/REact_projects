8f4b07ee4533b045734d561039543c82635ef18d
srilaxmipillalamarri <srilaxmip15@gmail.com>
2023-05-08 12:29:41 +0530

    first commit

---
+++ react-full-course/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Final from "./HigherOrderComp/final/ClickCounter";
+import Final2 from "./HigherOrderComp/final/HoverCounter";
+
+function App() {
+  return (
+    <div className="App">
+      <Final name={"srikanth"} />
+      <Final2 />
+    </div>
+  );
+}
+
+export default App;
